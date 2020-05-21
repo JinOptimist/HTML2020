ea89c975d356e342b56152f840d760c11194ca3f
JinOptimist <88jin@mail.ru>
2020-05-21 21:05:24 +0300

    'FirstOurJs'

--- js/index.js
+++ js/index.js
@@ -12,4 +12,67 @@ $(document).ready(function(){
 		isAutoplay: true,
 		delayAutoplay: 10 * 1000
 	});
+	
+	
+	$(".showPreview").click(function(){
+		var userName = $("#login").val();
+		$('.placeForUserName').text(userName);
+		
+		var color = $("input[type=color]").val();
+		$('.preview').css("border-color", color);
+		
+		var birthdayString = $("input[type=date]").val();//"2000-12-05"
+		var birthday = new Date(birthdayString);//Sun May 21 2000 03:00:00
+		var now = new Date();//date now
+		var age = now.getFullYear() - birthday.getFullYear();
+		$('.placeForAge').text(age);
+		
+		
+		var sex = $("input[name=sex]:checked").val();
+		if (sex == 'm'){
+			$('.placeForSex').text('man');
+		}else{
+			$('.placeForSex').text('woman');
+		}
+		
+		
+		var size = $("#sizeInput").val();
+		if (size == 0){
+			$('#previewImg').attr('src', 'img/girl2.jpg' );
+		}else if (size == 1){
+			$('#previewImg').attr('src', 'img/girl3.jpg' );
+		}else if (size == 2){
+			$('#previewImg').attr('src', 'img/girl4.jpg' );
+		}else if (size == 3){
+			$('#previewImg').attr('src', 'img/girl5.jpg' );
+		}else if (size == 4){
+			$('#previewImg').attr('src', 'img/girl7.jpg' );
+		}else if (size == 5){
+			$('#previewImg').attr('src', 'img/girl6.jpg' );
+		}
+	});
+	
+	
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
